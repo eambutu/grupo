b483d434178f5f8a1ca1cad089f09a1f9f9315ea
Phillip <kip.phil@gmail.com>
2018-09-15 16:31:19 -0400

    Add functionality to button

--- src/client/components/Homepage.js
+++ src/client/components/Homepage.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import '../styles/Homepage.css';
 
 class Homepage extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+
+    this.createCart = () => {
+      // Some logic to go and create a cart, and should somehow return an ID
+      window.location = '/cart/1';
+    };
+  }
+
+  render() {
     return (
       <div className="center">
         <div className="title">grupo</div>
         <div className="button-container">
-          <button className="homepage-button">Create Order</button>
+          <button className="homepage-button" onClick={this.createCart}>Create Order</button>
         </div>
       </div>
     );
