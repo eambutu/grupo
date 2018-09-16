8842db6e7453c29544b94979a4b06df849907859
Phillip <kip.phil@gmail.com>
2018-09-15 21:01:10 -0400

    Style changes and bug fixes on server

--- src/server/index.js
+++ src/server/index.js
@@ -1,89 +1,80 @@
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
-	
-const MongoClient = require('mongodb').MongoClient;
-
-//MongoClient.connect('mongodb://localhost:27017/db', { useNewUrlParser: true }, (err, db) => {
-//    let databaseName = 'Grupo';
-//    let database = db.db(databaseName);
-   // carts = database.collection('carts');
-//    
-//});
-
-
-var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var mongoose = require("mongoose");
-mongoose.Promise=global.Promise;
-mongoose.connect("mongodb://localhost:27017/grupodb");
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/grupodb');
 
-var db = mongoose.connection;
+const db = mongoose.connection;
+
+let Cart;
+let Order;
+let Carts;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', function() {
-    //define schema
-    var item = new mongoose.Schema({
-      name: String, 
-      quantity: Number, 
-      size: String
-    });
+db.once('open', () => {
+  // define schema
+  const item = new mongoose.Schema({
+    name: String,
+    quantity: Number,
+    size: String
+  });
 
-    var orderSchema = new mongoose.Schema({
-      cartid: String,
-      name: String,
-      venmo: String,
-      items: [item]
-    });
+  const orderSchema = new mongoose.Schema({
+    cartid: String,
+    name: String,
+    venmo: String,
+    items: [item]
+  });
 
-    var cartSchema = new mongoose.Schema({
-      cartid: String,
-      orders: [orderSchema]
-    });
+  const cartSchema = new mongoose.Schema({
+    cartid: String,
+    orders: [orderSchema]
+  });
 
-    var Cart = mongoose.model("Cart", cartSchema);
+  Cart = mongoose.model('Cart', cartSchema);
 
-    var cartsSchema = new mongoose.Schema({
-      carts: [cartSchema]
-    });
+  const cartsSchema = new mongoose.Schema({
+    carts: [cartSchema]
+  });
 
-    //Models
-    var Order = mongoose.model("Order", orderSchema);
-    var Carts = mongoose.model("Carts", cartsSchema);
+  // Models
+  Order = mongoose.model('Order', orderSchema);
+  Carts = mongoose.model('Carts', cartsSchema);
 });
-db.createCollection("Carts");
+db.createCollection('Carts');
 
-app.post("/createCart", (req, res) => {
-    var newCart = new Cart(req.body);
-    newCart.save(function (error, newCart) {
-      if (err) return console.error(err);
-      // saved!
-    });
-});    
-
-app.post("/addOrder", (req, res) => {
-    var myData = new Order(req.body)
-    var currentCart = db.Carts.find({id: myData.cartId});
-    var currentCartItems = currentCart.orders;
-    var updatedCartItems = {$concatArrays: [currentCartItems, [myData]]};
-    db.Carts.findOneAndUpdate({"cartid": myData.cartId}, 
-        myData.save().then(item => {
-	    res.send("Order saved to database");
-        }).catch(err => {
-	    res.status(400).send("Unable to save to database");
-        }));
+app.post('/createCart', (req, res) => {
+  const newCart = new Cart(req.body);
+  newCart.save((err) => {
+    if (err) return console.error(err);
+    // saved!
+    return null;
+  });
 });
 
-app.get("/getCart", (req, res) => {
-    // TODO: implement
-    res.sendFile();
+app.post('/addOrder', (req, res) => {
+  const myData = new Order(req.body)
+  const currentCart = Carts.find({ id: myData.cartId });
+  const currentCartItems = currentCart.orders;
+  const updatedCartItems = {$concatArrays: [currentCartItems, [myData]]};
+  db.Carts.findOneAndUpdate({ cartid: myData.cartId },
+    myData.save().then(item => {
+      res.send("Order saved to database");
+    }).catch(err => {
+      res.status(400).send('Unable to save to database');
+    }));
 });
 
-//app.use(express.static('dist'));
+app.get('/getCart', (req, res) => {
+  // TODO: implement
+  res.sendFile();
+});
 
 app.listen(8080, () => console.log('Listening on port 8080!'));
