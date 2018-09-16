657a7d67ca20dd5b62ca7af83bc35044c6be959f
Allen Lee <allenlee@mit.edu>
2018-09-15 20:23:07 -0400

    selection page

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Homepage from './Homepage';
 import Cart from './Cart';
+import NestedGrid from './Selection';
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Homepage} />
       <Route exact path="/cart/:cartId" component={Cart} />
-      <Route path="/selection/:selectionId" component={Selection} />
+      <Route exact path="/selection/:selectionId" component={NestedGrid} />
     </Switch>
   </Router>
 )
--- src/client/components/Selection.js
+++ src/client/components/Selection.js
@@ -1,13 +1,92 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import ComplexGrid from './OrderCard';
+import '../styles/OrderCard.css'
 
-class Selection extends Component {
-    render() {
-        return (
-            // item with a name, description, checkbox
-            <Card className={classes.card}>
-            </Card>
-        );
-    }
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    padding:100,
+  },
+  paper: {
+    padding: theme.spacing.unit,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+function FormRow(props) {
+  const { classes } = props;
+
+  return (
+    <React.Fragment>
+      <Grid item xs={4}>
+            <ComplexGrid ItemName="Milk Bubble Tea"
+                           ItemDescription="Refreshing, sweet, and chewy!">
+            </ComplexGrid>
+      </Grid>
+      <Grid item xs={4}>
+            <ComplexGrid ItemName="Milk Bubble Tea"
+                           ItemDescription="Refreshing, sweet, and chewy!">
+            </ComplexGrid>
+      </Grid>
+      <Grid item xs={4}>
+            <ComplexGrid ItemName="Milk Bubble Tea"
+                           ItemDescription="Refreshing, sweet, and chewy!">
+            </ComplexGrid>
+      </Grid>
+    </React.Fragment>
+  );
 }
+
+FormRow.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+function NestedGrid(props) {
+  const { classes } = props;
+
+  return (
+      <div>
+          <h1 style={{marginTop:50, marginBottom:-50}}>Kung Fu Tea
+      </h1>
+      <div className="centerframe">
+        <div className={classes.root}>
+        <Grid container spacing={8}>
+            <Grid item xs={12} container spacing={24}>
+            <FormRow classes={classes} />
+            </Grid>
+            <Grid item xs={12} container spacing={24}>
+            <FormRow classes={classes} />
+            </Grid>
+            <Grid item xs={12} container spacing={24}>
+            <FormRow classes={classes} />
+            </Grid>
+        </Grid>
+        </div>
+    </div>
+    <div>
+        <Button style={{marginTop:-150}} variant="outlined" size="large" color="primary" className={classes.button}>
+            Submit
+        </Button>
+    </div>
+    </div>
+  );
+}
+
+NestedGrid.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(NestedGrid);
+
+
+
+// <div className="top-padding">
+                //     <ComplexSelection ItemName="Milk Bubble Tea"
+                //                     ItemDescription="Refreshing, sweet, and chewy!">
+                //     </ComplexSelection>
+                // </div>
