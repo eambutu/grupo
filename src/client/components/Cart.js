f63246d52a953cc8fc05c1542e6f2f5b04ad19bb
Phillip <kip.phil@gmail.com>
2018-09-16 03:09:59 -0400

    Make cart and order selection under one component

--- src/client/components/Cart.js
+++ src/client/components/Cart.js
@@ -1,85 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/Cart.css';
 
-class Cart extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      cartId: 0,
-      orders: [
-        {
-          user: 'Jeff',
-          items: [
-            {
-              name: 'Black milk tea',
-              quantity: 2,
-              size: 'M'
-            },
-          ]
-        },
-        {
-          user: 'Allen',
-          items: [
-            {
-              name: 'Green milk tea',
-              quantity: 3,
-              size: 'L'
-            },
-            {
-              name: 'Black milk tea',
-              quantity: 1,
-              size: 'M'
-            }
-          ]
-        }
-      ]
-    };
-  }
-
-  componentDidMount() {
-    if (this.props.match.params.cartId) {
-      this.setState({ cartId: this.props.match.params.cartId });
-    }
-  }
-
-  render () {
-    console.log(Object.values(this.state.orders)[0].user);
-    return (
-      <div>
-        <div className="title">Link: {this.state.cartId}</div>
-        <div className="button-container">
-          <button className="button">Add Order</button>
-        </div>
-        <div>
-          <table className="center">
-            <tbody className="orders-table">
-              {Object.values(this.state.orders).map(order => (
-                <tr className="orders-row-container">
-                  <div className="orders-row">
-                    <td className="orders-element">
-                      {order.user}
-                    </td>
-                    <td className="orders-element">
-                      <table>
-                        <tbody>
-                          {Object.values(order.items).map(item => (
-                            <tr>
-                              {item.name}, {item.quantity}, {item.size}
-                            </tr>
-                          ))}
-                        </tbody>
-                      </table>
-                    </td>
-                  </div>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+export default function Cart(props) {
+  return (
+    <div>
+      <div className="title">Link: {props.cartId}</div>
+      <div className="button-container">
+        <button className="button" onClick={props.addOrder}>Add Order</button>
       </div>
-    );
-  }
+      <div>
+        <table className="center">
+          <tbody className="orders-table">
+            {Object.values(props.orders).map(order => (
+              <tr className="orders-row-container">
+                <div className="orders-row">
+                  <td className="orders-element">
+                    {order.name}, Venmo: @{order.venmo}
+                  </td>
+                  <td className="orders-element">
+                    <table>
+                      <tbody>
+                        {Object.values(order.items).map(item => (
+                          <tr>
+                            {item.name}, {item.quantity}, {item.size}
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </td>
+                </div>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
-
-export default Cart;
--- src/server/index.js
+++ src/server/index.js
@@ -90,11 +90,10 @@ app.post('/deleteCart', (req, res) => {
 
 app.get('/getCart', (req, res) => {
   console.log('in get Cart');
-  const cart = new Cart(req.body);
   var cartsColl = db.collection('carts');
-  var orders = cartsColl.find({"cartid" : cart.cartid}, {"orders":1, _id:0}).toArray(function(err, results) { 
+  var orders = cartsColl.find({"cartid" : req.query.cartid}, {"orders":1, _id:0}).toArray(function(err, results) {
     console.log(results);
-    res.jsonp(results);
+    res.json(results);
   });
 });
 
